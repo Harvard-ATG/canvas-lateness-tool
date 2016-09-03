7e813c994eefefbeca599c70342eb94ac2b5e535
Arthur Barrett <abarrett@fas.harvard.edu>
2016-09-03 17:55:52 -0400

    Implemented spreadsheet export with submission deltas and accumulated lateness.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # canvas-lateness-tool
 Exports and aggregates late student submission data
+
+
+## Quickstart
+
+Setup python virtual environment:
+
+```sh
+$ virtualenv pyenv
+$ source pyenv/bin/activate
+$ pip install -r requirements.txt
+```
+
+Setup environment settings and update the `OAUTH_TOKEN` value, which is required to interact with the Canvas API:
+
+```sh
+$ cp -v .env.example .env
+$ nano .env
+```
+
+Run the script on a given canvas course ID:
+
+```sh
+$ python canvas_lateness.py
+usage: canvas_lateness.py [-h] [--use_cache] [--debug] course_id
+```
+
--- canvas_lateness.py
+++ canvas_lateness.py
@@ -22,6 +22,7 @@ def api_get_students_list(request_context, course_id):
     
     https://canvas.instructure.com/doc/api/courses.html#method.courses.users
     '''
+    logging.info("API fetching students for course: %s" % course_id)
     results = get_all_list_data(request_context, courses.list_users_in_course_users, course_id, "email", enrollment_type="student")
     students = sorted([{"sortable_name":x['sortable_name'], "id": x['id']} for x in results], key=lambda x: x['sortable_name'])
     logging.debug("Students in course: %s" % students)
@@ -33,6 +34,7 @@ def api_get_assignments_list(request_context, course_id):
 
     https://canvas.instructure.com/doc/api/assignments.html#method.assignments_api.index 
     '''
+    logging.info("API fetching assignments for course: %s" % course_id)
     results = get_all_list_data(request_context, assignments.list_assignments, course_id, '')
     logging.debug("Assignments List: %s" % [r['id'] for r in results]) 
     return results
@@ -43,6 +45,7 @@ def api_get_submissions(request_context, course_id, assignment_ids):
 
     https://canvas.instructure.com/doc/api/submissions.html#method.submissions_api.index
     '''
+    logging.info("API fetching submissions for course %s and assignment_ids: %s" % (course_id, ', '.join(map(str, assignment_ids))))
     include = "assignment"
     results = []
     for assignment_id in assignment_ids:
@@ -51,77 +54,250 @@ def api_get_submissions(request_context, course_id, assignment_ids):
         results.append({"assignment_id": assignment_id, "submissions": list_data})
     return results
 
-def fetch_data(course_id):
-    '''
-    Fetches students, assignments, and submissions from the canvas API.
-    '''
-    request_context = RequestContext(OAUTH_TOKEN, CANVAS_API_URL, per_page=100)
-    students = api_get_students_list(request_context, course_id)
-    assignments = api_get_assignments_list(request_context, course_id)
-    submissions = api_get_submissions(request_context, course_id, [a['id'] for a in assignments])
-    data = {
-        'assignments': assignments,
-        'submissions': submissions,
-        'students': students,
-    }
-    return data
-
-def load_data(course_id, cache_file, use_cache=False):
+def load(course_id, cache_file, use_cache=False):
     '''
     Loads data from local cache or from the Canvas API.
-
-    Only loads from the cache if the cache data is available
-    and if use_cache=True.
     '''
-    data = None
-    if use_cache and os.path.exists(cache_file):
-        logging.info("Loading data from %s instead of fetching from %s" % (cache_file, CANVAS_API_URL))
-        with open(cache_file, 'r') as f:
-            data = json.load(f)
-    else:
-        logging.info("Fetching data from %s" % CANVAS_API_URL)
-        data = fetch_data(course_id)
-        save_json(filename=cache_file, data=data)
+    data = {}
+    savecache = lambda: cache_write(cache_file, data)
+    if use_cache:
+        data = cache_read(cache_file) or {}
+
+    request_context = RequestContext(OAUTH_TOKEN, CANVAS_API_URL, per_page=100)
+    if 'students' not in data:
+        data['students'] = api_get_students_list(request_context, course_id)
+        savecache()
+    if 'assignments' not in data:
+        data['assignments'] = api_get_assignments_list(request_context, course_id)
+        savecache()
+    if 'submissions' not in data:
+        data['submissions'] = api_get_submissions(request_context, course_id, [a['id'] for a in data['assignments']])
+        savecache()
+
     return data
 
-def save_json(filename=None, data=None):
+def process(data):
+    '''
+    Transforms the raw data retrieved from the API into data about each student's "lateness"
+    for each assignment. The data is returned in a format suitable for output. 
+    '''
+
+    student_dict = dict([(s['id'], s['sortable_name']) for s in data['students']])
+    assignment_ids = [a['id'] for a in data['assignments']]
+    zero_delta = datetime.timedelta(days=0, seconds=0, microseconds=0)
+
+    # group submissions by assignment and student for easy lookup
+    submissions_by_assignment = {}
+    submissions_by_student = {}
+    for sub_item in data['submissions']:
+        assignment_id = sub_item['assignment_id']
+        if assignment_id not in assignment_ids:
+            continue
+        assignment_subs = sorted(sub_item['submissions'], key=lambda s: (s['user_id'], s['assignment_id'], s['submitted_at']))
+        submissions_by_assignment[assignment_id] = assignment_subs
+        for assignment_sub in assignment_subs:
+            submissions_by_student.setdefault(assignment_sub['user_id'], {}).setdefault(assignment_id, []).append(assignment_sub)
+
+    # aggregate the results
+    results = []
+    display_date_format = '%a, %b %d at %I:%M%p'
+    for student in sorted(data['students'], key=lambda s: s['sortable_name']):
+        student_id = student['id']
+        student_result = {
+            'student_id': student_id, 
+            'student_name': student['sortable_name'], 
+            'assignments': [],
+        }
+        total_lateness = zero_delta
+        for assignment in sorted(data['assignments'], key=lambda a: (a.get('assignment_group_id', 0), a['position'])):
+            assignment_id = assignment['id']
+            assignment_subs = submissions_by_assignment[assignment_id]
+
+            # skip when the assignment has no submissions or has no due date, because
+            # in either case there's no time delta to compare 
+            if len(assignment_subs) == 0 or not assignment['due_at']:
+                continue
+
+            student_assignment_subs = submissions_by_student[student_id].get(assignment_id, [])
+
+            # parse the assignment due date
+            due_date_iso = assignment['due_at']
+            due_date = None
+            due_date_display = ''
+            if due_date_iso:
+                due_date = dateutil.parser.parse(due_date_iso).replace(tzinfo=UTC_TZ)
+                due_date_display = due_date.astimezone(EST_TZ).strftime(display_date_format)
+
+            # parse the date of the last submission attempt
+            sub_date_iso = ''
+            sub_date = None
+            sub_date_display = ''
+            if len(student_assignment_subs) > 0:
+                sub_item = student_assignment_subs[-1]
+                sub_date_iso = sub_item['submitted_at']
+                if sub_date_iso:
+                    sub_date = dateutil.parser.parse(sub_date_iso).replace(tzinfo=UTC_TZ)
+                    sub_date_display = sub_date.astimezone(EST_TZ).strftime(display_date_format)
+
+            # calculate the delta
+            time_delta = None
+            time_delta_compact = ''
+            time_delta_seconds = ''
+            if due_date and sub_date:
+                time_delta = sub_date - due_date
+                time_delta_compact = '{days}d{seconds}s'.format(days=time_delta.days, seconds=time_delta.seconds)
+                time_delta_seconds = '%d' % time_delta.total_seconds()
+                
+            # accumulate the total lateness
+            if time_delta is not None and time_delta > zero_delta:
+                total_lateness += time_delta
+
+            # add assignment delta 
+            student_result['assignments'].append({
+                'assignment_id': assignment['id'],
+                'assignment_name': assignment['name'],
+                'due_date_iso': due_date_iso,
+                'due_date_display': due_date_display,
+                'submission_date_iso': sub_date_iso,
+                'submission_date_display': sub_date_display,
+                'time_delta_compact': time_delta_compact,
+                'time_delta_seconds': time_delta_seconds,
+            })
+
+        # update total delta
+        student_result['total_lateness_compact'] = '{days}d{seconds}s'.format(days=total_lateness.days, seconds=total_lateness.seconds)
+        student_result['total_lateness_seconds'] = '%d' % total_lateness.total_seconds()
+        student_result['total_lateness_hours'] = '%d' % int(total_lateness.total_seconds() / 3600) # round down to be nice
+
+        results.append(student_result)
+
+    return results
+
+def cache_read(filename):
+    '''
+    Helper to read data from a JSON file.
+    '''
+    data = None
+    if os.path.exists(filename):
+        logging.info("Reading data from: %s" % filename)
+        with open(filename, 'r') as f:
+            data = json.load(f)
+        if isinstance(data, dict):
+            logging.debug("File contains top-level keys: %s" % ", ".join(data.keys()))
+    else:
+        logging.info("File does not exist: %s" % filename)
+    return data
+
+def cache_write(filename, data):
     '''
     Helper to save data to a JSON file.
     '''
-    if filename is None:
-        raise Exception("Filename is required")
-    logging.info("Writing data to %s" % filename)
+    logging.info("Writing data to: %s" % filename)
     with open(filename, 'w') as outfile:
         json.dump(data, outfile, sort_keys=True, indent=2)
+    return data
 
-def save_spreadsheet(filename=None, data=None):
+def create_spreadsheet(filename, data, results):
     '''
     Creates a spreadsheet with the data.
     '''
-    if filename is None:
-        raise Exception("Filename is required")
+    # Styles
+    bold_style = xlwt.easyxf('font: bold 1')
+    right_align = xlwt.easyxf("align: horiz right")
+    numeric_style = xlwt.easyxf('', '0')
+
+    student_fmt = u'{sortable_name} ({id})'
+    student_width = 256 * max([len(student_fmt.format(**s)) for s in data['students']])
+
+    # Create workbook
+    wb = xlwt.Workbook(encoding="utf-8")
+
+    # Worksheet #1 w/ Submission Time Deltas
+    ws = wb.add_sheet('Delta Sheet', cell_overwrite_ok=True)
+    ws.write(0,0, u'Assignment \u2192'.encode('utf-8'), right_align)
+    ws.write(1,0, u'Students \u2193'.encode('utf-8'), right_align)
+    ws.col(0).width = student_width
+
+    row = 2
+    for result in results:
+        student_name_str = student_fmt.format(sortable_name=result['student_name'], id=result['student_id']) 
+        ws.write(row, 0, student_name_str)
+
+        for asstpos, asst in enumerate(result['assignments']):
+            assignment_name_str = u'{name} ({id})'.format(name=asst['assignment_name'], id=asst['assignment_id'])
+            columns = (
+                (u'Due'.encode('utf-8'), asst['due_date_display']),
+                (u'Submitted'.encode('utf-8'), asst['submission_date_display']),
+                (u'Delta (seconds)'.encode('utf-8'), asst['time_delta_seconds']),
+            )
+            colstart = asstpos * len(columns) + 1
+
+            ws.write(0, colstart, assignment_name_str, bold_style)
+            for colpos, column in enumerate(columns):
+                ws.write(1, colstart+colpos, column[0], bold_style) # column header
+                ws.write(row, colstart+colpos, column[1]) # column value for student
+        row += 1
+
+    # Worksheet #2 w/ Cumulative Lateness
+    ws = wb.add_sheet('Lateness Sheet', cell_overwrite_ok=True)
+    ws.col(0).width = student_width
+    ws.write(0,0, u'Students'.encode('utf-8'), bold_style)
+
+    row = 2
+    for result in results:
+        student_name_str = student_fmt.format(sortable_name=result['student_name'], id=result['student_id']) 
+        ws.write(row, 0, student_name_str)
+        columns = (
+            (u'Total (compact)'.encode('utf-8'), result['total_lateness_compact']),
+            (u'Total (hours)'.encode('utf-8'), result['total_lateness_hours'], numeric_style),
+            (u'Total (seconds)'.encode('utf-8'), result['total_lateness_seconds'], numeric_style),
+        )
+        colstart = 1
+        for colpos, column in enumerate(columns):
+            ws.write(0, colstart+colpos, column[0], bold_style) # column header
+            ws.write(row, colstart+colpos, *column[1:]) # column value for student
+        row += 1
+
+    logging.info("Writing spreadsheet to: %s" % filename)
+    wb.save(filename)
 
 def main():
-    logging.basicConfig(filename='debug.log', level=logging.DEBUG)
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    logging.getLogger('').addHandler(console) # add to root handler
-
+    # get CLI args
     parser = argparse.ArgumentParser(description="Export lateness data for each student's assignment")
     parser.add_argument('course_id', help='Canvas Course ID')
     parser.add_argument('--use_cache',  dest='use_cache', action='store_true', help="Use cached data (if available) rather than fetching from the API")
+    parser.add_argument('--debug',  dest='debug', action='store_true', help="Log debug output")
     parser.set_defaults(use_cache=False)
+    parser.set_defaults(debug=False)
     args = parser.parse_args()
-    logging.debug("CLI args: %s" % args)
 
+    if not OAUTH_TOKEN:
+        raise Exception("OAUTH_TOKEN is required (check your .env file).")
+
+    # configure logging
+    loggingConfig = {'filename': 'output.log', 'level': logging.INFO}
+    if args.debug:
+        loggingConfig['level'] = logging.DEBUG
+    logging.basicConfig(**loggingConfig)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(loggingConfig['level'])
+    requests_log.propagate = True
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO) # only ever output info to the console
+    logging.getLogger('').addHandler(console) # add to root handler
+
+    # configure output filenames
+    today = datetime.date.today().isoformat().replace('-','')
     cache_file = os.path.join(BASE_PATH, "%s-cache.json" % args.course_id)
-    data = load_data(args.course_id, cache_file, use_cache=args.use_cache)
+    results_json_file = os.path.join(BASE_PATH, "%s-results-%s.json" % (args.course_id, today))
+    results_xls_file = os.path.join(BASE_PATH, "%s-results-%s.xls" % (args.course_id, today))
 
-    spreadsheet_filename = os.path.join(BASE_PATH, "%s-export.xls" % args.course_id)
-    save_spreadsheet(filename=spreadsheet_filename, data=data)
+    # fetch and process the data
+    logging.debug("Begin.")
+    data = load(args.course_id, cache_file, use_cache=args.use_cache)
+    results = process(data)
+    cache_write(results_json_file, results)
+    create_spreadsheet(results_xls_file, data, results)
     logging.debug("Done.")
 
 if __name__ == "__main__":
